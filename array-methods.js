f3bef61e4ea57198e4a3c917d05a92d2a793ff90
MarjolijnMuller <polle.marjolijn@gmail.com>
2024-10-30 15:38:07 +0100

    array methods af

--- array-methods.js
+++ array-methods.js
@@ -2,6 +2,8 @@
 // Verwijder het laatste item van onderstaande array en log dit in de terminal
 // Log daarna de originele array om te kijken of deze waarde ook echt verwijderd is en de array nu korter is geworden
 // ---- Verwachte uitkomst: 'Vanille extract' en [ 'Eieren', 'Bloem', 'Bakpoeder' ]
+console.log("");
+console.log("Opdracht 0:");
 
 const cookieIngredients = ["Eieren", "Bloem", "Bakpoeder", "Vanille extract"];
 
@@ -12,32 +14,46 @@ console.log(cookieIngredients);
 /* Opdracht 1: */
 // Voeg een nieuw item toe aan onderstaande array en log de gehele array in de terminal om te kijken is het gelukt is
 // ---- Verwachte uitkomst: [ 'Hawai', 'Margherita', 'Salami' ]
+console.log("");
+console.log("Opdracht 1:");
 
 const pizzas = ["Hawai", "Margherita"];
-
+pizzas.push("Salami");
+console.log(pizzas);
 
 
 /* Opdracht 2: */
 // Verwijder het éérste item van onderstaande array en log dit in de terminal
 // Log daarna de originele array om te kijken of deze waarde ook echt verwijderd is en de array nu korter is geworden
 // ---- Verwachte uitkomst: 'Noten' en [ 'Eieren', 'Bloem', 'Cacao', 'Chocoladestukjes', 'Bakpoeder' ]
+console.log("");
+console.log("Opdracht 2:");
 
 const brownieIngredients = ["Noten", "Eieren", "Bloem", "Cacao", "Chocoladestukjes", "Bakpoeder"];
-
+const firstIngredient = brownieIngredients.shift();
+console.log(firstIngredient);
+console.log(brownieIngredients);
 
 /* Opdracht 3: */
 // Draai de volgorde van onderstaande array met namen om en log de gehele array in de terminal om te kijken is het gelukt is
 // ---- Verwachte uitkomst: [ 'Piet', 'Jan', 'Henk' ]
+console.log("");
+console.log("Opdracht 3:");
 
 const names = ["Henk", "Jan", "Piet"];
 
+console.log(names.reverse());
 
 
 /* Opdracht 4: */
 // Onderstaande array bevat een lijst met plaatsnamen. Voeg de naam "Amsterdam" toe tussen Utrecht en Maastricht.
 // ---- Verwachte uitkomst: [ 'Utrecht', 'Amsterdam', 'Maastricht', 'Eindhoven' ]
+console.log("");
+console.log("Opdracht 4:");
 
 const cities = ["Utrecht", "Maastricht", "Eindhoven"];
 
+cities.splice(1, 0, "Amsterdam");
+console.log(cities);
 
 
